31008abae98fd37fb6b04616dd4479f836bfbf0a
Phelipe Perboires <phelipeperboires@gmail.com>
2026-02-11 21:47:46 -0300

    Migration e atualizacao para .net 10

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
-                       "Server=(localdb)\\mssqllocaldb;Database=CotaZap;Trusted_Connection=True;MultipleActiveResultSets=true";
+                       "Server=.;Database=CotaZap;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
